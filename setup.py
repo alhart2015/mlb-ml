b4b3c2a84e4198ca77c6b13fcc037269d8620a91
alhart2015 <alhart@davidson.edu>
2018-10-11 20:49:13 -0400

    Rename mlb dir mlb-ml

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mlb-ml",
+    version="0.0.1",
+    author="Alden Hart",
+    author_email="not.putting.this.in@nope.com",
+    description="Use Tensorflow to predict MLB performance",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alhart2015/mlb-ml",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
