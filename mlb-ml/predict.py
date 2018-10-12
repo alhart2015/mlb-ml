286e34fe544adf2a9a526c0bff69e398a830432d
alhart2015 <alhart@davidson.edu>
2018-10-11 21:02:01 -0400

    Try to set up pytest ugh

---
+++ mlb-ml/predict.py
@@ -0,0 +1,12 @@
+"""
+Entry point to predict stuff.
+"""
+
+def this_is_a_test():
+    print("ointoiu")
+
+def main():
+    this_is_a_test()
+
+if __name__ == '__main__':
+    main()
---
+++ mlb-ml/tests/test_predict.py
@@ -0,0 +1,6 @@
+"""
+Tests on the main script predict.py
+"""
+
+def test_the_test():
+    this_is_a_test()
