ce2c8375efd7d43dc61e8ef5adfa69a9ff359ecd
alhart2015 <alhart@davidson.edu>
2018-10-11 21:38:55 -0400

    Imports work now

--- mlb-ml/predict.py
+++ mlb-ml/predict.py
@@ -3,12 +3,15 @@ Entry point to predict stuff.
 """
 from api import gameday_api_handler
 
+import utils
+
 def this_is_a_test():
     return 2
 
 def main():
     this_is_a_test()
     gameday_api_handler.test()
+    print(utils.HITTER_STAT_URL)
 
 
 if __name__ == '__main__':
